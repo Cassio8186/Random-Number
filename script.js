673594ba6a1359bce849d1437ec0f927561aee09
cassio8186 <cassio8186@gmail.com>
2019-09-26 17:06:17 -0300

    Add function rollNumber
    Started to create function, it works but needs
    to implement timeout or another function

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+var num1 = document.querySelector("#num1");
+var num2 = document.querySelector("#num2");
+var number = document.querySelector("#randomNumber");
 function generateRandom() {
 
-    var num1 = document.querySelector("#num1");
-    var num2 = document.querySelector("#num2");
-    var random = document.querySelector("#randomNumber");
+
     setRightOrder(num1, num2);
-    random.textContent = randomNumber(num1, num2);
+    var randomNumber = randomNum(num1, num2);
+    rollNumber(number.textContent, randomNumber);
+
 
 }
-function randomNumber(min, max) {
+function randomNum(min, max) {
     var min = (parseInt(min.value) - 1);
     var max = (parseInt(max.value) + 1);
     var random = ((Math.floor(Math.random() * (max - min)) + min));
-    console.log(random);
-
+    console.log("random: " + random);
     return random;
 }
 
@@ -23,4 +25,20 @@ function setRightOrder(num1, num2) {
         num1.value = value2;
         num2.value = value1;
     }
+}
+
+function rollNumber(number1, number2) {
+    start = parseInt(number1)
+    end = parseInt(number2);
+    if (start < end) {
+        while (start != (end + 1)) {
+            number.textContent = start++
+        }
+    }
+    else if (start > end) {
+        while (start != (end - 1)) {
+            number.textContent = start--
+        }
+    } else { number.textContent = start }
+
 }
