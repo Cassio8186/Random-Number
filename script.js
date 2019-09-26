b9161977ff0e59e370638ec84f60780980274342
cassio8186 <cassio8186@gmail.com>
2019-09-26 11:05:34 -0300

    start writing Functions
    Started writing functions that will be used to
    generate randonm numbers

---
+++ script.js
@@ -0,0 +1,15 @@
+function generateRandom() {
+
+    var num1 = document.querySelector("#num1");
+    var num2 = document.querySelector("#num2");
+    setRightorder(num1, num2);
+    var randomNumber = randomNumber(num1, num2);
+
+}
+function randomNumber(num1, num2) {
+
+}
+
+function setRightOrder(num1, num2) {
+
+}
