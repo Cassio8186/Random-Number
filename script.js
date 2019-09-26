6bcca90082ba0b7344f632225e889a3c3d7cd67f
Cassio <cassio8186@gmail.com>
2019-09-26 12:44:37 -0300

    fix stupid bug

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ function generateRandom() {
 
     var num1 = document.querySelector("#num1");
     var num2 = document.querySelector("#num2");
-    setRightorder(num1, num2);
-    var randomNumber = randomNumber(num1, num2);
-
+    setRightOrder(num1,num2);
 }
 function randomNumber(num1, num2) {
 
 }
 
 function setRightOrder(num1, num2) {
-    /**bugadown */
-    const value1 = parseInt(num1.value);
-    const value2 = parseInt(num2.value);
-    if (value2 > value1) {
+    const value1 = num1.value;
+    const value2 = num2.value;
+    if (value1 > value2) {
         num1.value = value2;
         num2.value = value1;
     }
 }
+generateRandom();
